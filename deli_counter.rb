64322f63c17a416d151487c6ce2629ddb7610140
Nethelbert <gilblasse@gmail.com>
2019-11-04 19:32:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,7 @@ def line(katz_deli)
   line = katz_deli.each_with_index.map {|name,i| "#{i + 1}. #{name}"}
   empty_line = "The line is currently empty."
   active_line = "The line is currently: #{line.join(' ')}"
+  
   puts katz_deli.size == 0 ? empty_line : active_line
 end
 
