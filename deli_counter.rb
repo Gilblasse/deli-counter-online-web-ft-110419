bbfc90e863ff287904822914527d2fce64997391
Nethelbert <gilblasse@gmail.com>
2019-11-04 19:30:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,10 @@
 require 'pry'
 
 def line(katz_deli)
-  
- if katz_deli.size == 0
-   puts "The line is currently empty."
- else
-    line = katz_deli.each_with_index.map {|name,i| "#{i + 1}. #{name}"}
-    puts "The line is currently: #{line.join(' ')}"
- end
+line = katz_deli.each_with_index.map {|name,i| "#{i + 1}. #{name}"}
+empty_line = "The line is currently empty."
+active_line = "The line is currently: #{line.join(' ')}"
+
+katz_deli.size == 0 ? puts empty_line : puts active_line
+
 end
