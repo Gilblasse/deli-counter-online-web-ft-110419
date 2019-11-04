f640d4d93b5b472491b72b3eb9203edca775a7ef
Nethelbert <gilblasse@gmail.com>
2019-11-04 19:23:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,6 @@ def line(katz_deli)
  if katz_deli.size == 0
    puts "The line is currently empty."
  else
-   katz_deli.each_with_index.map {|name,i| }
+   katz_deli.each_with_index.map {|name,i| "#{i + 1}. #{name}"}
  end
 end
